17caab0dac8192503974fe1f0a5a31f422657bd2
hectorsvill <hectorstevenvillasano@gmail.com>
2019-11-18 20:57:16 -0800

    fetch status

--- cs-build-week-2/ViewController.swift
+++ cs-build-week-2/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let n = NetworkController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        n.getStatus()
+        
     }
 
 
