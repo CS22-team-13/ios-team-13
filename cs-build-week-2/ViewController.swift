7e8745ca39c4e15c7968f79fb2e394f2e34fac02
hectorsvill <hectorstevenvillasano@gmail.com>
2019-11-18 20:33:13 -0800

    Initial Commit

---
+++ cs-build-week-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cs-build-week-2
+//
+//  Created by Hector on 11/18/19.
+//  Copyright © 2019 Hector. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
